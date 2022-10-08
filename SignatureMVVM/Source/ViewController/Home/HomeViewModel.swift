5ea1448a8fca45032604c5b7b623abce97992ea1
kimjinu <kimjinu5555@gmail.com>
2022-10-08 15:03:37 +0900

    [Add] HomeViewModel 추가

--- SignatureMVVM/Source/ViewController/Home/HomeViewModel.swift
+++ SignatureMVVM/Source/ViewController/Home/HomeViewModel.swift
@@ -5,4 +5,35 @@
 //  Created by 김진우 on 2022/10/08.
 //
 
-import Foundation
+final class HomeViewModel: ViewModelType {
+    
+    struct Dependency {
+        
+    }
+    
+    struct Payload {
+        
+    }
+    
+    enum Input {
+        case loginButtonTap
+    }
+    
+    enum Output {
+        case presentAlert
+    }
+    
+    var dependency: Dependency
+    var payload: Payload
+    
+    var input: Input
+    var output: Output
+    
+    init(dependency: Dependency, payload: Payload) {
+        self.dependency = dependency
+        self.payload = payload
+        
+        input = .loginButtonTap
+        output = .presentAlert
+    }
+}
