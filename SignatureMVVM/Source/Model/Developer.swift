84d783ff24755383541575fcf16fd9c49d88c506
kimjinu <kimjinu5555@gmail.com>
2022-10-16 17:39:41 +0900

    [Add] 테스트용 Developer타입 정적 상수 추가 (iOS / Window / Fullstack Develoepr)

--- SignatureMVVM/Source/Model/Developer.swift
+++ SignatureMVVM/Source/Model/Developer.swift
@@ -10,4 +10,11 @@ import Foundation
 struct Developer {
     let name: String
     let isiOSDeveloper: Bool
+    let mainDevice: String?
+}
+
+extension Developer {
+    static let iOSDeveloper: Developer = .init(name: "GAMZA", isiOSDeveloper: true, mainDevice: "MacBook Pro M1")
+    static let WindowDeveloper: Developer = .init(name: "Chris Kim", isiOSDeveloper: false, mainDevice: nil)
+    static let FullstackDeveloper: Developer = .init(name: "Green Kim", isiOSDeveloper: false, mainDevice: nil)
 }
